e3c00e25819a93edd2b10ec917db28e0e55eaab4
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-01-13 21:06:34 +0700

    Update User_Controller.php

--- app/Http/Controllers/User_Controller.php
+++ app/Http/Controllers/User_Controller.php
@@ -11,4 +11,21 @@ class User_Controller extends Controller
             'title' => 'User'
         ]);
     }
+
+    public function store(Request $request){
+        $validasi = request()->validate([
+            'nama' => 'required|string|min:4|max:200',
+            'username' => 'required|string|min:4|max:200',
+            'password' => 'required|string|min:4',
+        ]);
+        if($validasi == false){
+            return redirect()->back()->withErrors($validasi)->withInput();
+        }
+        $user = new User();
+        $user->nama = $request->input('nama');
+        $user->username = $request->input('username');
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+        return redirect()->route('user-index')->with('success','User berhasil ditambahkan');
+    }
 }
