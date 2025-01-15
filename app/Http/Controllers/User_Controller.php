075b6dd0a6e55830727cbd9d60777145623275ec
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-01-15 21:07:46 +0700

    Update User_Controller.php

--- app/Http/Controllers/User_Controller.php
+++ app/Http/Controllers/User_Controller.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+
 class User_Controller extends Controller
 {
     public function index(){
+        $user = User::all();
         return view('user',[
-            'title' => 'User'
+            'title' => 'User page',
+            'user' => $user
         ]);
     }
 
     public function store(Request $request){
         $validasi = request()->validate([
-            'nama' => 'required|string|min:4|max:200',
+            'name' => 'required|string|min:4|max:200',
+            'email' => 'required|string|min:5',
             'username' => 'required|string|min:4|max:200',
             'password' => 'required|string|min:4',
         ]);
@@ -22,10 +26,41 @@ class User_Controller extends Controller
             return redirect()->back()->withErrors($validasi)->withInput();
         }
         $user = new User();
-        $user->nama = $request->input('nama');
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
         $user->username = $request->input('username');
         $user->password = bcrypt($request->input('password'));
         $user->save();
         return redirect()->route('user-index')->with('success','User berhasil ditambahkan');
+    }
+
+    public function destroy($id){
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->route('user-index')->with('success', 'User berhasil dihapus.');
+    }
+
+    public function update(Request $request,$id){
+        $validasi = request()->validate([
+            'name' => 'required|string|min:4|max:200',
+            'email' => 'required|string|min:5',
+            'username' => 'required|string|min:4|max:200',
+            'password' => 'nullable|string|',
+        ]);
+        if($validasi == false){
+            return redirect()->back()->withErrors($validasi)->withInput();
+        }
+        $user = User::find($id);
+        $user->nama = $request->input('name');
+        $user->username = $request->input('username');
+        $user->email = $request->input('email');
+        // $user->password = bcrypt($request->input('password'));
+
+        if ($request->filled('password')) {
+            $user->password = bcrypt($request->input('password'));
+        }
+
+        $user->save();
+        return redirect()->route('user-index')->with('success','User berhasil diupdate');
     }
 }
