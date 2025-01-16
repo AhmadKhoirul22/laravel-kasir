b30a84aa7d615d3bbb03cba00f908022678d46ec
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-01-16 15:01:28 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,10 @@
 @extends('layout.template')
 @section('content')
-<h1>selamat datang di dashboard</h1>
+<div class="intro-y flex items-center mt-5 mb-5">
+    <h1 class="font-bold text-lg">{{ $title }}</h1>
+</div>
+<div class="grid grid-cols-12">
+    <div class="intro-y col-span-12 lg:col-span-12">
+    </div>
+</div>
 @endsection
