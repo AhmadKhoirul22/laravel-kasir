9657d96b9ec8c4b75e78ed541f072d9a29ab86d5
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-01-18 20:15:17 +0700

    Update _css.blade.php

--- resources/views/layout/_css.blade.php
+++ resources/views/layout/_css.blade.php
@@ -1,10 +1,13 @@
 <meta charset="utf-8">
-<link href="midone/dist/images/logo.svg" rel="shortcut icon">
+<link href="/midone/dist/images/logo.svg " rel="shortcut icon">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Midone admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
 <meta name="keywords" content="admin template, Midone admin template, dashboard template, flat admin template, responsive admin template, web app">
 <meta name="author" content="LEFT4CODE">
-<title>Dashboard - Midone - Tailwind HTML Admin Template</title>
+<title>{{ $title }}</title>
 <!-- BEGIN: CSS Assets-->
-<link rel="stylesheet" href="midone/dist/css/app.css" />
+<link rel="stylesheet" href="/midone/dist/css/app.css" />
 <!-- END: CSS Assets-->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
