20db4092b9a3dbb01a3a1666177715dc5f94b85b
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-01-31 21:47:29 +0700

    Update Kategori_Controller.php

--- app/Http/Controllers/Kategori_Controller.php
+++ app/Http/Controllers/Kategori_Controller.php
@@ -37,7 +37,7 @@ class Kategori_Controller extends Controller
 
     public function update(Request $request,$id){
         $validasi = request()->validate([
-            'kategori_nama' => 'required|string|min:4|max:200',
+            'kategori_nama' => 'required|string|min:4|max:200|unique:kategori,kategori_nama',
         ]);
         if($validasi == false){
             return redirect()->back()->withErrors($validasi)->withInput();
