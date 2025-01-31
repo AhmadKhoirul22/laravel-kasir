5020fef8e6940338072f0ad6d8d8e6d0c8c04a06
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-01-31 21:47:08 +0700

    Update User_Controller.php

--- app/Http/Controllers/User_Controller.php
+++ app/Http/Controllers/User_Controller.php
@@ -17,7 +17,7 @@ class User_Controller extends Controller
 
     public function store(Request $request){
         $validasi = request()->validate([
-            'name' => 'required|string|min:4|max:200',
+            'name' => 'required|string|min:4|max:200|unique:users,name',
             'email' => 'required|string|min:5',
             'username' => 'required|string|min:4|max:200',
             'password' => 'required|string|min:4',
