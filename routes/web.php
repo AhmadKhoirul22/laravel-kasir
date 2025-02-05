36bf0b0537f063064d2a3e4fbcce3c0b6f2163c8
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-02-05 14:59:01 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth_Controller;
 use App\Http\Controllers\Dashboard_Controller;
 use App\Http\Controllers\Kategori_Controller;
 use App\Http\Controllers\Produk_Controller;
@@ -20,6 +21,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/login',[Auth_Controller::class,'index'])->name('login');
+
 
 Route::get('/',[Dashboard_Controller::class,'index'])->name('dashboard');
 
