02b751829d772b04895ab72ffbc411ac9812b1f5
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-02-14 22:40:43 +0700

    Update Dashboard_Controller.php

--- app/Http/Controllers/Dashboard_Controller.php
+++ app/Http/Controllers/Dashboard_Controller.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Dashboard_Controller extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
     public function index(){
         return view('dashboard',[
-            'title' => 'Dashboard Page',
+            'title' => 'Dashboard page',
         ]);
     }
 }
