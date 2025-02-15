168b20755746fb244d3b2a2837ad2542fd79d940
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-02-15 10:29:49 +0700

    Update user.blade.php

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -110,7 +110,8 @@
                             <div class="flex sm:justify-center items-center">
                                 <a class="flex items-center mr-3" href="javascript:;" data-toggle="modal"
                                     data-target="#large-modal-size-preview<?= $uu['id'] ?>">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit mx-auto"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+                                    <i data-feather="edit" ></i>
+                                    {{-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit mx-auto"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg> --}}
                                     Edit </a>
                                 <div>
                                     <form action="{{ route('user.destroy', $uu['id']) }}" method="post" id="delete-form-{{ $uu['id'] }}">
