c147a39c51705d35459472693744f4429b23a9de
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-02-16 21:16:44 +0700

    Update _top_bar.blade.php

--- resources/views/layout/_top_bar.blade.php
+++ resources/views/layout/_top_bar.blade.php
@@ -17,8 +17,8 @@
             <div class="dropdown-box mt-10 absolute w-56 top-0 right-0 z-20">
                 <div class="dropdown-box__content box bg-theme-38 text-white">
                     <div class="p-4 border-b border-theme-40">
-                        <div class="font-medium">Denzel Washington</div>
-                        <div class="text-xs text-theme-41">Frontend Engineer</div>
+                        <div class="font-medium">{{ Auth::user()->name }}</div>
+                        <div class="text-xs text-theme-41">Admin</div>
                     </div>
                     <div class="p-2">
                         <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 rounded-md"> <i data-feather="user" class="w-4 h-4 mr-2"></i> Profile </a>
@@ -27,7 +27,10 @@
                         <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 rounded-md"> <i data-feather="help-circle" class="w-4 h-4 mr-2"></i> Help </a>
                     </div>
                     <div class="p-2 border-t border-theme-40">
-                        <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 rounded-md"> <i data-feather="toggle-right" class="w-4 h-4 mr-2"></i> Logout </a>
+                        <form action="{{ route('logout') }}" method="post" >
+                        @csrf
+                        <button type="submit" onclick="return confirm('yakin untuk logout?')" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 rounded-md"> <i data-feather="toggle-right" class="w-4 h-4 mr-2"></i> Logout </button>
+                        </form>
                     </div>
                 </div>
             </div>
